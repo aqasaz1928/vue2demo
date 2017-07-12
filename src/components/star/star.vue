2a33560f522d46c4f8fc9695419873bcc8a09a66
aqasaz1928 <aqasaz1928@gmail.com>
2017-07-12 22:14:23 +0800

    add star component

---
+++ src/components/star/star.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="star" :class="starType">
+    <span v-for="(itemClass,index) in itemClasses" :key="index" :class="itemClass" class="star-item">
+
+    </span>
+</div>
+  
+</template>
+<script type="text/ecmascript-6">
+export default {
+  props: {
+      size: {
+          type: Number
+      },
+      score: {
+          type: Number
+      }
+  },
+  computed : {
+      starType () {
+          return 'star-'+this.size;
+      }
+  }
+}
+</script>
+<style rel="stylesheet/stylus" lang="stylus">
+.star
+    font-size 0
+    .star-item
+        display inline-block
+        background-repeat no-repeat
+        background-size 100%
+    &.star-48
+        .star-item
+            width 20px
+            height 20px
+            margin-right 22px
+    &.star-36
+    &.star-24
+
+</style>
+

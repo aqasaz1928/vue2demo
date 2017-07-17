5bbef4998c98ae81375f2dd161ea2d9cb96a826c
aqasaz1928 <aqasaz1928@gmail.com>
2017-07-17 17:26:09 +0800

    add better-scroll to realize the scroll menu and food

--- src/components/food-item/food.vue
+++ src/components/food-item/food.vue
@@ -1,18 +1,16 @@
 <template>
 <div class="food">
     <div class="icon">
-        <img :src="foodData.icon">
+        <img :src="foodData.icon" width="57px" height="57px">
     </div>
     <div class="content">
         <h2 class="name">{{foodData.name}}</h2>
-        <p class="desc">{{foodData.description}}</p>
+        <p class="desc" v-if="foodData.description">{{foodData.description}}</p>
         <div class="extra">
-            <span>月售{{foodData.sellCount}}份</span>
-            <span>好评率{{foodData.rating}}%</span>
+            <span class="count">月售{{foodData.sellCount}}份</span><span>好评率{{foodData.rating}}%</span>
         </div>
         <div class="price">
-            <span>￥{{foodData.price}}</span>
-            <span v-if="foodData.oldPrice">￥{{foodData.oldPrice}}</span>
+            <span class="now">￥{{foodData.price}}</span><span class="old" v-if="foodData.oldPrice">￥{{foodData.oldPrice}}</span>
         </div>
     </div>
 </div>
@@ -28,7 +26,43 @@ export default {
 }
 </script>
 <style rel="stylesheet/stylus" lang="stylus">
-
+.food
+    display flex
+    .icon
+        flex 0 0 57px
+        width 57px
+        height 57px
+        margin-right 10px
+    .content
+        flex 1
+        font-size 0
+        position relative
+        .name
+            font-size 14px
+            line-height 14px
+            margin-top 2px
+            margin-bottom 8px
+            color rgb(7,17,27)
+        .desc,.extra
+            font-size 10px
+            color rgb(147,153,159)
+            line-height 10px
+        .desc
+            margin-bottom 8px
+        .extra
+            .count
+                margin-right 12px
+        .price
+            font-weight 700
+            line-height 24px
+            .now
+                font-size 14px
+                color rgb(240,20,20)
+                margin-right 8px
+            .old
+                font-size 10px
+                color rgb(147, 153, 159)
+                text-decoration line-through
 
 </style>
 
--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="goods">
-<div class="menu-wrapper">
+<div class="menu-wrapper" ref="menuWrapper">
     <ul v-if="goods">
         <li v-for="(item, index) in goods" :key="index" class="menu-item">
             <span class="text border-1px"><icon :size="3" :type="iconMap[item.type]" v-if="item.type>0"></icon>
@@ -8,11 +8,12 @@
         </li>
     </ul>
 </div>
-<div class="foods-wrapper">
+<div class="foods-wrapper" ref="foodWrapper">
     <ul>
         <li class="food-list" v-for="(item, index) in goods" :key="index">
+            <h1 class="title">{{item.name}}</h1>
             <ul>
-                <li v-for="(food, i) in item.foods" :key="i" class="food-item">
+                <li v-for="(food, i) in item.foods" :key="i" class="food-item border-1px">
                     <food :foodData="food"></food>
                 </li>
             </ul>
@@ -26,7 +27,8 @@
  import Const from '@/common/js/const'
  import icon from '@/components/icon/icon'
  import food from '@/components/food-item/food'
- 
+ import BScroll from 'better-scroll'
+
 export default {
   props: {
       seller: {
@@ -45,6 +47,9 @@ export default {
             let repData = rep.body
             if (repData.errno === Const.ERR_OK) {
                 this.goods = repData.data
+                this.$nextTick(() => {
+                    this._initScroll()
+                })
             }
           },
           rep => {
@@ -55,6 +60,12 @@ export default {
   components: {
       icon,
       food
+  },
+  methods: {
+      _initScroll () {
+          this.menuScroll = new BScroll(this.$refs.menuWrapper, {})
+          this.foodScroll = new BScroll(this.$refs.foodWrapper, {})
+      }
   }
 }
 </script>
@@ -91,6 +102,21 @@ export default {
                 border-1px(rgba(7,17,27,0.1))
     .foods-wrapper
         flex 1
+        .food-list
+            .title
+                height 26px
+                padding-left 14px
+                font-size 12px
+                line-height 26px
+                color rgb(147,153,159)
+                background-color #f3f5f7
+                border-left 2px solid #d9dde1
+            .food-item
+                padding 18px 0
+                margin 0 18px
+                border-1px(rgba(7,17,27,.1))
+                &:last-child
+                    border-none()
 
 </style>
 

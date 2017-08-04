f34c54948b8e1538952144d846f3a7f011c004a9
aqasaz1928 <aqasaz1928@gmail.com>
2017-08-04 17:40:59 +0800

    2017-08-04 update

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -2,15 +2,30 @@
 <div class="ratings">
     <div class="ratings-content">
         <div class="overview">
-            <div class="overview-left"></div>
+            <div class="overview-left">
+                <h1 class="score">{{seller.score}}</h1>
+                <div class="title">综合评分</div>
+                <div class="rank">高于周边商家{{seller.rankRate}}%</div>
+            </div>
             <div class="overview-right"></div>
         </div>
+        <split></split>
     </div>
 </div>
   
 </template>
 <script type="text/ecmascript-6">
+import split from '@/components/split/split' 
+
 export default {
+    props: {
+        seller: {
+            type: Object
+        }
+    },
+    components: {
+        split
+    }
   
 }
 </script>
@@ -30,8 +45,25 @@ export default {
             width 137px
             border-right 1px solid rgba(7,17,27,0.1)
             text-align center
+            .score
+                font-size 24px
+                line-height 28px
+                color rgb(255, 153, 0)
+                margin-bottom 4px
+            .title
+                font-size 12px
+                line-height 12px
+                color rgb(7,17,27)
+                margin-bottom 8px
+            .rank
+                font-size 10px
+                line-height 10px
+                color rgb(147,153,159)
+                margin-bottom 6px
         .overview-right
             flex 1
+            padding-left 24px
+            
 
 </style>
 
